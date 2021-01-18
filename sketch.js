cc1bd0b5bb5743e879dd677d7d92336b28fa0ee0
gamingbro-123 <73818869+gamingbro-123@users.noreply.github.com>
2021-01-18 14:07:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball;
+var database;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    database = firebase.database();
+
+    var ballref = database.ref("ball/position");
+    ballref.on("value", readData)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+
+    database.ref("ball/position").set({x:ball.x,y:ball.y})
+}
+
+function readData(DATA)
+{
+    var position = DATA.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+
+
